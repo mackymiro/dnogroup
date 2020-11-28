d22e4d89a655e58eff1436bee86650c166cc538b
mackymiro <ask.macky.miro@gmail.com>
2020-11-28 15:25:00 +0800

    add updates in dong fang

--- app/Http/Controllers/DongFangCorporationController.php
+++ app/Http/Controllers/DongFangCorporationController.php
@@ -2617,6 +2617,7 @@ class DongFangCorporationController extends Controller
             'user_id'=>$user->id,
             'pv_id'=>$id,
             'date'=>$request->get('date'),
+            'invoice_number'=>$request->get('invoiceNo'),
             'account_name_no'=>$request->get('accountNameNo'),
             'cheque_number'=>$request->get('chequeNumber'),
             'cheque_amount'=>$request->get('chequeAmount'),
